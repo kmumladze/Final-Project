39288ced3d9b3b1ef993dff25458ec540f4e24fd
kmumladze <ketevanmumladze17@gmail.com>
2024-03-12 23:38:38 +0400

    fix selectedValue bug

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -5,34 +5,39 @@ import { HttpService } from '../shared/http.service';
 @Component({
   selector: 'app-hotel-list',
   templateUrl: './hotel-list.component.html',
-  styleUrl: './hotel-list.component.css'
+  styleUrl: './hotel-list.component.css',
 })
 export class HotelListComponent implements OnInit {
-hotels: Hotel[] = [];
-public hotelId!: any
-// @Input() selectedHotel: any
-selectedValue: string = '';
+  hotels: Hotel[] = [];
+  public hotelId!: any;
+  // @Input() selectedHotel: any
+  selectedValue: string = '';
 
-constructor(private httpService: HttpService){}
+  constructor(private httpService: HttpService) {}
 
-ngOnInit(): void {
-  console.log(this.selectedValue)
-  this.httpService.getHttp().subscribe((data: any) => {    
-    this.hotels = data
-    console.log(this.hotels)
-  });
-}
+  ngOnInit(): void {
+    console.log(this.selectedValue);
+    this.httpService.getHttp().subscribe((data: any) => {
+      this.hotels = data;
+      console.log(this.hotels);
+    });
+  }
 
-onBtnSave(id:any) {
-  this.hotelId = id;
-  console.log(this.hotelId)
-}
+  onBtnSave(id: any) {
+    this.hotelId = id;
+    console.log(this.hotelId);
+  }
 
-
-onSelectChange(event: any) {
+  onSelectChange(event: any) {
     this.selectedValue = event.currentTarget.value;
-     console.log(event.currentTarget.value)
-    }
+    console.log(event.currentTarget.value);
+  }
 
+  getSelectedHotel() {
+    if (this.selectedValue === '') return this.hotels;
 
+    return this.hotels.filter(
+      (hotel) => hotel.id === Number(this.selectedValue)
+    );
+  }
 }
