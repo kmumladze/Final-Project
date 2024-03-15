59159bf0071fd28257228b63e07351c62eef1145
kmumladze <ketevanmumladze17@gmail.com>
2024-03-15 17:39:36 +0400

    fixed burger icon

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  
-  public isCardShow:Boolean = false;
+  public isCardShow: Boolean = false;
 
-  showCard(){
-      this.isCardShow = true;
+  toggleCard() {
+    this.isCardShow = !this.isCardShow;
   }
 }
